bb937cdc3a557be78a5a8717ef31e6418003e1be
Elo Trolla <elo.trolla@tptlive.ee>
2023-06-05 19:41:26 +0300

    Add models

--- src/Controllers/PublicController.php
+++ src/Controllers/PublicController.php
@@ -2,8 +2,14 @@
 
 namespace App\Controllers;
 
+use App\Models\Post;
+
+
 class PublicController {
     public function index(){
+
+        $result = Post::all();
+        dd($result); // dump() die() koos
         $name = 'Nimi';
         view('index', compact('name'));   
     }
